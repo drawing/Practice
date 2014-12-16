232cd2039e3438f13fadaad933e1f8f2b0543700
bodeng <drawing@users.noreply.github.com>
2014-12-16 19:20:50 +0800

    add lambda

---
+++ Language/C++11/lambda.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> vec;
+	vec.push_back(9);
+	vec.push_back(1);
+	vec.push_back(1);
+
+	int out = 0;
+	
+	// can not access out var
+	std::for_each(vec.begin(), vec.end(), [](int v){ cout << v << endl;});
+	cout << endl;
+
+	// can access out but cannot modify it
+	std::for_each(vec.begin(), vec.end(), [=](int v){ cout << v << ":" << out << endl;});
+	cout << endl;
+
+	// can modify it
+	std::for_each(vec.begin(), vec.end(), [&](int v){ cout << v << ":" << out++ << endl;});
+	cout << endl;
+
+	// out is reference other is value
+	std::for_each(vec.begin(), vec.end(), [=, &out](int v){ cout << v << ":" << out++ << endl;});
+	cout << endl;
+
+	std::for_each(vec.begin(), vec.end(), [](int & v){ cout << v++ << ":"; cout << v << endl;});
+	cout << endl;
+
+	return 0;
+}
+
