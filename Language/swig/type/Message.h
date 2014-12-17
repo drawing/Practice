b651ef434c396febd61911998cf816a66f2a0365
bodeng <drawing@users.noreply.github.com>
2014-12-17 20:26:26 +0800

    add swig type

---
+++ Language/swig/type/Message.h
@@ -0,0 +1,11 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+class Message {
+public:
+	void SetTime(time_t update);
+	void SetSeq(int32_t seq);
+};
+
+#endif // MESSAGE_H
+
---
+++ Language/swig/type/message.i
@@ -0,0 +1,13 @@
+
+%module drawing
+
+
+%{
+#include "message.h";
+%}
+
+typedef unsigned long time_t;
+typedef int int32_t;
+
+%include "Message.h";
+
