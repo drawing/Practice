6464c25674e02278717f0ddc3eff79fb493224ae
bodeng <drawing@users.noreply.github.com>
2014-12-24 17:11:11 +0800

    update move

--- Language/C++11/move.cpp
+++ Language/C++11/move.cpp
@@ -1,27 +1,55 @@
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Elem {
 public:
-	Elem() {
-		cout << "Create One Elem" << endl;
+	Elem(const char * name) : name(strdup(name)) {
+		cout << "Create One Elem: " << name << endl;
 	}
-	Elem(const Elem & elem) {
-		cout << "Copy One Elem" << endl;
+	Elem(const Elem & other) {
+		free(name);
+		name = strdup(other.name);
+		cout << "Create Elem By Other: " << name << endl;
 	}
+	Elem(Elem && rf) {
+		free(name);
+		name = rf.name;
+		rf.name = nullptr;
+		cout << "Create Elem By R-Refer: " << name << endl;
+	}
+public:
+	char * name;
 };
 
-Elem func1() {
-	Elem e1;
-	return std::move(e1);
+bool is_right_reference(Elem & elem)
+{
+	return false;
+}
+bool is_right_reference(Elem && elem)
+{
+	return true;
 }
 
 int main()
 {
-	Elem result;
-	result = func1();
+	string one = "TestOne String";
+	string two = std::move(one);
+
+	cout << "One:" << one << endl;
+	cout << "Two:" << two << endl;
+
+	Elem elem("one");
+	cout << is_right_reference(Elem("temp")) << endl;
+	cout << is_right_reference(elem) << endl;
+	cout << is_right_reference(std::move(elem)) << endl;
+
+	Elem elem1 = elem;
+	Elem elem2 = std::move(elem);
+
+	cout << (long)elem.name << endl;
 	return 0;
 }
 
