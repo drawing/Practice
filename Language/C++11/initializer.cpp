de58b2f0c9e8c99a980e9035b2ade6d503303f39
bodeng <drawing@users.noreply.github.com>
2014-12-24 20:51:58 +0800

    update init

--- Language/C++11/initializer.cpp
+++ Language/C++11/initializer.cpp
@@ -2,6 +2,18 @@
 #include <set>
 #include <iostream>
 
+using namespace std;
+
+class TestInit
+{
+public:
+	TestInit(initializer_list<int> list) {
+		for (auto k : list) {
+			cout << "TEST:" << k << endl;
+		}
+	}
+};
+
 int main()
 {
 	std::vector<int> vec = {1, 2, 3, 4};
@@ -11,5 +23,7 @@ int main()
 	std::cout << std::endl;
 
 	std::set<int> st = {1, 2, 3, 4};
+
+	TestInit a = {1, 3, 5, 7};
 	return 0;
 }
